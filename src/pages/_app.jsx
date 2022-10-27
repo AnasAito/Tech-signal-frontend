8200f043b056fe738b1e730701a1a2aecb3638b9
AnasAito <khdimaty@gmail.com>
2022-10-27 18:56:05 +0100

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,12 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
